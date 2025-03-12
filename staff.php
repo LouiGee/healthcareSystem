91ea926362e7222ab8cac2eef6018c69291a2bb7
Luis Goate <lg565@kent.ac.uk>
2025-03-12 10:57:21 +0000

    staff php pulling through server side data and login working

--- staff.php
+++ staff.php
@@ -1,12 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appointments</title>  
+</head>
+<body>
 <?php
 	$host = 'dragon.ukc.ac.uk';
 	$dbname = 'lg565';
 	$user = 'lg565';
 	$pwd = 'rles3ev';
 
+	$name = htmlspecialchars($_POST['name']);
+    $staffID = htmlspecialchars($_POST['staffID']);
 
-    echo '<h1> COMP 8870 Healthcare </h1>'; 
-    echo ' <div class="divider"> <h4> <b>Doctor Information:</b> Displaying information for <h4> </div> ';
+
+    echo "<h1> COMP 8870 Healthcare </h1>"; 
+    echo " <div class='divider'> <h4> <b>Doctor Information:</b> Displaying information for $name <h4> </div> ";
 
 	try {
 		$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pwd);
@@ -18,7 +29,7 @@
                 ON a.pID = p.pID
                 LEFT JOIN Staff s
                 ON a.sID = s.sID 
-                WHERE sName =   ;";
+                WHERE sName = '$name';";
 		$handle = $conn->prepare($sql);
 		$handle->execute();
 		$res = $handle->fetchAll();
@@ -34,3 +45,6 @@
 		echo "PDOException: ".$e->getMessage();
 	}
 ?>
+
+</body>
+</html>
